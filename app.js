8e16fb264f4613a3c9abd30839dbdaa07267c5c4
CaChiaradia <cachiaradia.dg@hotmail.com>
2021-11-25 17:14:49 -0300

    Realização do CRUD

--- app.js
+++ app.js
@@ -26,27 +26,31 @@ const Cliente = mongoose.model("cadastrocliente", tabela);
 
 
 app.get("/", (req,res)=>{
+
     Cliente.find((erro, dados)=>{
         if (erro) {
-            res.status(400).send({erro:`Ocorreu um erro durante o processamento da sua requisição ${erro}`});
+            res.status(400).send({output:`Ocorreu um erro durante o processamento da sua requisição ${erro}`});
             return
     }
-    res.status(200).send({resultado:dados});
+    res.status(200).send({output:'Clintes Cadastrados', resultado:dados});
     })
 });
 
 
-app.post("/cadastro", (req,res)=>{
+app.post("/cadastro",(req,res)=>{
+
     const dados = new Cliente(req.body);
-    dados.save().then(()=>{
-        res.status(201).send({retorno:"Dados Cadastrados com sucesso"})
-    }).catch((erro)=>res.status(400).send({error: `Erro ao tentar cadastrar ${erro}`}));    
+    dados.save().then((dt)=>{
+        res.status(201).send({output:"Dados Cadastrados com sucesso", payload:dt})
+    }).catch((erro)=>res.status(400).send({output:`Erro ao tentar cadastrar ${erro}`}));   
+
 });
 
 app.put("/atualizar/:id", (req,res)=>{
-    Cliente.findByAndUpdate(req.params.id,res.body,{new:True},(erro, dados)=>{
+
+    Cliente.findByAndUpdate(req.params.id,req.body,{new:True},(erro, dados)=>{
         if (erro){
-            res.status(400).send({erro:`Erro ao tentar atualizar ${error}`});
+            res.status(400).send({output:"Dados Atualizados", payload:dados});
             return
         }
         res.status(200).send({resultado:dados});
@@ -55,23 +59,21 @@ app.put("/atualizar/:id", (req,res)=>{
 });
 
 app.delete("/apagar/:id", (req,res)=>{
+
     Cliente.findByIdAndDelete(req.params.id,(erro,dados)=>{
         if (erro){
-            res.status(400).send({error:`Erro ao tentar apagar ${erro}`})
+            res.status(400).send({output:`Erro ao tentar apagar ${erro}`})
             return
         }
         res.status(204).send({resultado:"Cliente apagado"});
     })
-    
-    
-    
+
 });
 
 app.use((req,res)=>{
     res.type("application/json");
     res.status(404).send('404 - Not Found');    
-    
-    
+
 });
 
 app.listen(3000,()=>console.log(`Servidor online na porta 3000. Pata encerrar tecle CTRL+C`))
